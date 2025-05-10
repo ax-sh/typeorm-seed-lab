31748eb61a2c74baec5c5c6b4da6fb34a9621c12
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-05-11 00:31:45 +0545

    refactor: readability

---
+++ src/options/datasource.options.ts
@@ -0,0 +1,19 @@
+import type { DataSourceOptions } from "typeorm";
+import type { SeederOptions } from "typeorm-extension";
+import { User } from "../entities/user.entity.ts";
+import { Post } from "../entities/post.entity.ts";
+import { seederOptions } from "./seeder.options.ts";
+import { env } from "../env.ts";
+
+const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = env;
+export const options: DataSourceOptions & SeederOptions = {
+  // type: "mysql", // NOTE works for both
+  type: "postgres",
+  host: DB_HOST || "localhost",
+  port: Number(DB_PORT) || 3306,
+  username: DB_USER || "test",
+  password: DB_PASSWORD || "test",
+  database: DB_NAME || "test",
+  entities: [User, Post],
+  ...seederOptions,
+};
--- src/options/seeder.options.ts
+++ src/options/seeder.options.ts
@@ -1,7 +1,7 @@
 import type { SeederOptions } from "typeorm-extension";
-import { UsersFactory } from "./factories/users.factory.ts";
-import { PostsFactory } from "./factories/post.factory.ts";
-import { MainSeeder } from "./main.seeder.ts";
+import { UsersFactory } from "../factories/users.factory.ts";
+import { PostsFactory } from "../factories/post.factory.ts";
+import { MainSeeder } from "../main.seeder.ts";
 
 export const seederOptions: SeederOptions = {
   // additional config options from SeederOptions( typeorm-extension)
